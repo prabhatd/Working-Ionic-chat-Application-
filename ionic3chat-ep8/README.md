d7720f9b42cd7d2c6dfc5ee4fe3ae8d23ddda7e0
Prabhat Dwivedi <prabhatd@pune-com-dt-014.payso.in>
2019-08-14 12:45:18 +0530

    Working Ionic Private Chat Application without error

---
+++ ionic3chat-ep8/README.md
@@ -0,0 +1,10 @@
+# Ionic 3 Chat
+
+## Episode 8
+
+### One2One Chat
+
+The code for the eigth part of Ionic 3 chat app series. For further details - http://tphangout.com/?p=671
+
+Thanks.
+
---
+++ ionic3chat-ep8/src/app/app.firebaseconfig.ts
@@ -0,0 +1,8 @@
+export var config = {
+    apiKey: "AIzaSyDMzi2ZSdZGjW4s1A-o-j4d0yXtV7S6Zek",
+    authDomain: "ionicprivatechat.firebaseapp.com",
+    databaseURL: "https://ionicprivatechat.firebaseio.com",
+    projectId: "ionicprivatechat",
+    storageBucket: "",
+    messagingSenderId: "149984722882"
+  };
